e3e5cac23e2332a6fe6b4d3fa11062e781a30aa1
Max Keller <max.keller427@gmail.com>
2019-06-15 11:23:43 -0600

    got base code in, working through bugs

--- config/routes.js
+++ config/routes.js
@@ -1,32 +1,73 @@
-const axios = require('axios');
+const axios = require("axios");
+const jwt = require("jsonwebtoken");
+const secrets = require("./secrets.js");
+const bcrypt = require("bcryptjs");
+const db = require("./userModel.js");
 
-const { authenticate } = require('../auth/authenticate');
+const { authenticate } = require("../auth/authenticate");
 
 module.exports = server => {
-  server.post('/api/register', register);
-  server.post('/api/login', login);
-  server.get('/api/jokes', authenticate, getJokes);
+  server.post("/api/register", register);
+  server.post("/api/login", login);
+  server.get("/api/jokes", authenticate, getJokes);
 };
 
+function generateToken(user) {
+  jwt.sign(
+    {
+      userId: user.id,
+      userPass: user.pass
+    },
+    secrets.jwt,
+    { expiresIn: "1h" }
+  );
+}
+
 function register(req, res) {
-  // implement user registration
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 12);
+  user.password = hash;
+
+  db.add(user)
+    .then(newUser => {
+      const token = generateToken(newUser);
+
+      res
+        .status(201)
+        .json({ message: `Welcome ${newUser.username}`, authToken: token });
+    })
+    .catch(err => {
+      res.status(500).json({ message: "Please try again" });
+    });
 }
 
 function login(req, res) {
-  // implement user login
+  let { username, password } = req.body;
+
+  db.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+
+        res
+          .status(201)
+          .json({ message: `Welcome ${user.username}`, token: token });
+      }
+    });
 }
 
 function getJokes(req, res) {
   const requestOptions = {
-    headers: { accept: 'application/json' },
+    headers: { accept: "application/json" }
   };
 
   axios
-    .get('https://icanhazdadjoke.com/search', requestOptions)
+    .get("https://icanhazdadjoke.com/search", requestOptions)
     .then(response => {
       res.status(200).json(response.data.results);
     })
     .catch(err => {
-      res.status(500).json({ message: 'Error Fetching Jokes', error: err });
+      res.status(500).json({ message: "Error Fetching Jokes", error: err });
     });
 }
